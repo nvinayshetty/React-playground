825e322bebb2c8278a967dbc07b30a2029273ed8
vinay <nvinayshetty@gmail.com>
2024-08-04 16:49:01 +0800

    setting up components and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
+import {CORE_CONCEPTS} from './data.js'
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx'
+
+
 function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core concept</h2>
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[1]}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[2]}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[3]}
+                        />
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
